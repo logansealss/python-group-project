bdbc64dac7ee56128e8143a776dd934456bb6bf1
JohnCarrera <johnrcarrera@gmail.com>
2022-10-15 01:26:37 -0400

    merge forward

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -62,6 +62,7 @@ def sign_up():
     form = SignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
+        print(form.data)
         user = User(
             first_name=form.data['first_name'],
             last_name=form.data['last_name'],
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -21,6 +21,8 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+    first_name = StringField('firstName', validators=[DataRequired()])
+    last_name = StringField('lastName', validators=[DataRequired()])
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
