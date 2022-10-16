368d3d82475af5ea6992e2ed40aeb8b0028440dd
JohnCarrera <johnrcarrera@gmail.com>
2022-10-16 19:01:22 -0400

    added basic styling and organization to task row items

--- react-app/src/components/TaskApp/taskRowItem.js
+++ react-app/src/components/TaskApp/taskRowItem.js
@@ -2,19 +2,27 @@ import React from 'react';
 import moveIcon from '../../img/ellipsis-vertical.svg'
 import './taskRowItem.css';
 
-export default function TaskRowItem({task}) {
-  return (
-    <div className='tri-main-div'>
-        <img className='tri-move-grab' src={moveIcon}/>
-        <div className='tri-prio-indi'></div>
-        <input
-            className='tri-checkbox'
-            type='checkbox'
-        />
-        <div className='tri-task-name'>
-            {task.name}
-        </div>
-    </div>
+export default function TaskRowItem({ task }) {
+    return (
+        <div className='tri-main-div'>
+            <img className='tri-move-grab' src={moveIcon} />
+            <div className='tri-prio-indi'></div>
+            <div className='tri-checkbox-div'>
+                <input
+                    className='tri-checkbox'
+                    type='checkbox'
+                />
+            </div>
+            <div className='tri-inner-grp'>
 
-  )
+                <div className='tri-task-name'>
+                    {task.name}
+                </div>
+                <div className='tri-tags'>
+                    tag
+                </div>
+            </div>
+        </div>
+
+    )
 }
