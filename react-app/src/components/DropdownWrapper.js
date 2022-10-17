1546387f8739c5a36471dd0d30c769605101c043
fisjac <jfisher9882@gmail.com>
2022-10-16 20:05:58 -0500

    added dropdown wrapper

---
+++ react-app/src/components/DropdownWrapper.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+import './dropdown.css'
+
+export default function DropDownWrapper(props) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  return (
+    <>
+    {React.cloneElement(
+      props.children,
+      {
+        'onClick': openMenu,
+        'style': {'cursor': 'pointer'}
+      }
+      )}
+    {showMenu && (
+      <div
+        className='dropdown_container'
+        style={{'top': props.offset}}
+        >
+        <div className='dropdown_menu'>Sample Menu Box</div>
+        {props.menu}
+      </div>)}
+    </>
+  );
+};
