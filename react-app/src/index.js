1e18b6da1ba789bb7de5f1e73a0e1138efa105d7
fisjac <jfisher9882@gmail.com>
2022-10-17 12:03:50 -0500

    added modal logic

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+
+
 import App from './App';
 import configureStore from './store';
+import { ModalProvider } from './context/Modal';
+import './index.css';
+
 
 const store = configureStore();
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-        <App />
+    <ModalProvider>
+      <Provider store={store}>
+          <App />
       </Provider>
+    </ModalProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
