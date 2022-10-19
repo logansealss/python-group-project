1db8dcf730d4436ad554607941bb15a6a7f56d2e
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-10-19 00:48:48 -0500

    implemented list detail panel

--- react-app/src/components/TaskApp/listDetailPanel.js
+++ react-app/src/components/TaskApp/listDetailPanel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
@@ -10,20 +10,58 @@ function getDateFromToday(daysForward = 0) {
   res = new Date(res);
   res.setDate(res.getDate() + daysForward)
   res = res.toISOString();
-  res = res.replace('T', ' ')
   res = res.slice(0, 10)
   return res;
 }
 
-function getListDetails(tasks, startDate, dueDate) {
+function getListDetailsFromList(tasks, listId) {
 
   let tasksToCheck = Object.values(tasks)
 
   let result = {
-    tasks: 0,
+    totalTasks: 0,
     overdueTasks: 0,
     completedTasks: 0,
-    estimatedTime: 0
+    estimatedTime: 0,
+    tasks: []
+  }
+
+  const currentDate = getDateFromToday();
+
+  return tasksToCheck.reduce((result, task) => {
+
+    let taskDueDate = task.dueDate ? task.dueDate.slice(0, 10) : task.dueDate
+
+    if (task.listId === listId) {
+
+      result.totalTasks++;
+      result.tasks.push(task)
+
+      if (taskDueDate < currentDate) {
+        result.overdueTasks++;
+      }
+      if (task.completed) {
+        result.completedTasks++;
+      }
+      if (task.duration > 0) {
+        result.estimatedTime += task.duration
+      }
+    }
+
+    return result
+  }, result)
+}
+
+function getListDetailsFromDates(tasks, startDate, dueDate) {
+
+  let tasksToCheck = Object.values(tasks)
+
+  let result = {
+    totalTasks: 0,
+    overdueTasks: 0,
+    completedTasks: 0,
+    estimatedTime: 0,
+    tasks: []
   }
 
   const currentDate = getDateFromToday();
@@ -35,7 +73,8 @@ function getListDetails(tasks, startDate, dueDate) {
     if (startDate === undefined
       || (taskDueDate >= startDate && taskDueDate <= dueDate)) {
 
-      result.tasks++;
+      result.totalTasks++;
+      result.tasks.push(task)
       if (task.completed) {
         result.completedTasks++;
       }
@@ -45,7 +84,8 @@ function getListDetails(tasks, startDate, dueDate) {
     }
 
     if (taskDueDate < currentDate && dueDate !== getDateFromToday(1) && !task.completed) {
-      result.tasks++;
+      result.totalTasks++;
+      result.tasks.push(task)
       result.overdueTasks++;
       if (task.duration > 0) {
         result.estimatedTime += task.duration
@@ -65,54 +105,91 @@ export default function ListDetailPanel() {
   let listDetails = null;
 
   if (listId === "all") {
-    listDetails = getListDetails(tasks)
+    listDetails = getListDetailsFromDates(tasks)
     listDetails.name = "All Tasks"
   } else if (listId === "today") {
-    listDetails = getListDetails(tasks, getDateFromToday(), getDateFromToday())
+    listDetails = getListDetailsFromDates(tasks, getDateFromToday(), getDateFromToday())
     listDetails.name = "Today"
   } else if (listId === "tomorrow") {
-    listDetails = getListDetails(tasks, getDateFromToday(1), getDateFromToday(1))
+    listDetails = getListDetailsFromDates(tasks, getDateFromToday(1), getDateFromToday(1))
     listDetails.name = "Tomorrow"
   } else if (listId === "week") {
-    listDetails = getListDetails(tasks, getDateFromToday(), getDateFromToday(6))
+    listDetails = getListDetailsFromDates(tasks, getDateFromToday(), getDateFromToday(6))
     listDetails.name = "This Week"
+  } else {
+
+    let list = lists && Object.values(lists).find(lst => lst.id === +listId)
+
+    if (list) {
+      listDetails = getListDetailsFromList(tasks, +listId)
+      listDetails.name = list.name
+    }
+  }
+
+  if (listDetails) {
+    console.log(listDetails.tasks)
   }
 
   return (listDetails &&
     <div>
-      <div id="list-details-name">
-        {listDetails.name}
+      <div id='list-details-name-container'>
+        <div id="list-details-name">
+          {listDetails.name}
+        </div>
       </div>
       <div id="list-details-data-container">
         <div className="list-details-data">
           <div>
-            <div 
+            <div
               className='list-details-data-value'
               id='list-details-tasks'
-             >
-              {listDetails.tasks}
+            >
+              {listDetails.totalTasks}
             </div>
             <div className='list-details-data-label'>
               tasks
             </div>
           </div>
-        </div>
-        {!!listDetails.overdueTasks &&
-          <div class="list-details-data">
-            <div>
-              <div 
+          {!!listDetails.estimatedTime &&
+            <div
+              id="list-details-estimated-time"
+            >
+              <div
                 className='list-details-data-value'
-                id='list-details-overdue'
               >
-                {listDetails.overdueTasks}
+                <span>
+                  {Math.floor(listDetails.estimatedTime / 60)}
+                </span>
+                <small>
+                  {'hrs '}
+                </small>
+                <span>
+                  {listDetails.estimatedTime % 60}
+                </span>
+                <small>
+                  min
+                </small>
               </div>
               <div className='list-details-data-label'>
-                overdue
+                estimated
               </div>
             </div>
+          }
+        </div>
+        <div class="list-details-data">
+          <div>
+            <div
+              className='list-details-data-value'
+              id='list-details-overdue'
+            >
+              {listDetails.overdueTasks}
+            </div>
+            <div className='list-details-data-label'>
+              overdue
+            </div>
           </div>
-        }
-        <div 
+        </div>
+        <div
           className="list-details-data"
           id="list-details-completed"
         >
