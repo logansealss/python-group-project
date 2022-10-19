ccdd3ffa1378f1775fe85327011af702303f13ca
fisjac <jfisher9882@gmail.com>
2022-10-19 14:46:19 -0500

    fixed import bug

--- react-app/src/components/TaskApp/MainPanel/createTaskSubPanel.js
+++ react-app/src/components/TaskApp/MainPanel/createTaskSubPanel.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react'
-import Select from 'react-select';
-import ReactDOM from 'react-dom';
 import dueDateIcon from '../../../img/calendar-day.svg';
 import startDateIcon from '../../../img/square-caret-right.svg';
 import postponeIcon from '../../../img/calendar-plus.svg';
