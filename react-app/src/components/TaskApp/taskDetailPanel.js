02ed92c30053089d9a2e497a4a5d98b02348f07a
JohnCarrera <johnrcarrera@gmail.com>
2022-10-19 22:40:11 -0400

    basic styling complete for edit task

--- react-app/src/components/TaskApp/taskDetailPanel.js
+++ react-app/src/components/TaskApp/taskDetailPanel.js
@@ -4,21 +4,22 @@ import { useParams } from 'react-router-dom';
 import { getSingleTask } from '../../store/tasks';
 import { updateATask } from '../../store/tasks';
 import selectMenuTimes from '../../data/selectMenuTimes.json';
+import dueDateIcon from '../../img/calendar-day.svg';
+import startDateIcon from '../../img/square-caret-right.svg';
+import postponeIcon from '../../img/calendar-plus.svg';
+import prioIcon from '../../img/exclamation.svg';
+import listIcon from '../../img/list.svg';
+import repeatIcon from '../../img/rotate.svg';
+import clockIcon from '../../img/clock.svg';
+import locationPin from '../../img/location-dot.svg';
+import userIcon from '../../img/user.svg';
+import checkIcon from '../../img/check.svg';
+import tagIcon from '../../img/tag.svg';
+import minusIcon from '../../img/minus.svg';
+import EliIcon from '../../img/ellipsis.svg';
+import downCaret from '../../img/caret-down.svg';
+import './taskDetailPanel.css';
 
-// import dueDateIcon from '../../img.calendar-day.svg';
-// import startDateIcon from '../../img/square-caret-right.svg';
-// import postponeIcon from '../../img/calendar-plus.svg';
-// import prioIcon from '../../img/exclamation.svg';
-// import listIcon from '../../img/list.svg';
-// import repeatIcon from '../../img/rotate.svg';
-// import clockIcon from '../../img/clock.svg';
-// import locationPin from '../../img/location-dot.svg';
-// import userIcon from '../../img/user.svg';
-// import checkIcon from '../../img/check.svg';
-// import tagIcon from '../../img/tag.svg';
-// import minusIcon from '../../img/minus.svg';
-// import EliIcon from '../../img/ellipsis.svg';
-// import downCaret from '../../img/caret-down.svg';
 import './taskDetailPanel.css';
 
 export default function TaskDetailPanel() {
@@ -68,7 +69,7 @@ export default function TaskDetailPanel() {
     }, [task]);
 
 
-    const handleCtSubmit = (e) => {
+    const handleUtSubmit = (e) => {
         e.preventDefault();
 
         const data = {
@@ -87,112 +88,15 @@ export default function TaskDetailPanel() {
     return (task && lists && tags &&
         <div className='tad-main-panel'>
             <form
-                onSubmit={handleCtSubmit}
-                className='ctsp-form-el'
+                onSubmit={handleUtSubmit}
+                className='tad-form-el'
             >
                 <div
-                    className='ctsp-main-div'
+                    className='tad-main-div'
                 >
-                    {/* <div className='ctsp-ut-main-div'>
-                    <div className='ctsp-ut-grp-1'>
-                        <div className='ctsp-btn-div-solo-single ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={minusIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                    </div>
-                    <div className='ctsp-ut-grp-2'>
-                        <div className='ctsp-btn-div-left ctsp-ut-btn' >
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={checkIcon}
-                            />
-                        </div>
-                        <div className='ctsp-btn-div-center-solo ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={postponeIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                        <div className='ctsp-btn-div-rt ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={userIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                    </div>
-                    <div className='ctsp-ut-grp-3'>
-                        <div className='ctsp-btn-div-left ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={prioIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                        <div className='ctsp-btn-div-center-solo ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={dueDateIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                        <div className='ctsp-btn-div-center ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={listIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                        <div className='ctsp-btn-div-rt ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={tagIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                    </div>
-                    <div className='ctsp-ut-grp-4'>
-                        <div className='ctsp-btn-div-solo-single ctsp-ut-btn'>
-                            <img
-                                className='ctsp-ut-icon-style ctsp-ut-hover-style'
-                                src={EliIcon}
-                            />
-                            <img
-                                className='ctsp-ut-caret-style ctsp-ut-hover-style'
-                                src={downCaret}
-                            />
-                        </div>
-                    </div>
-                </div> */}
-                    {/* <div className='ctsp-ct-input-main'> */}
-                    <div className='ctsp-ct-pseudo-input'>
+                    <div className='tad-ct-pseudo-input'>
                         <input
-                            className='ctsp-ct-input'
+                            className='tad-ct-input'
                             type='text'
                             value={taskName}
                             onChange={(e) => setTaskName(e.target.value)}
@@ -204,98 +108,128 @@ export default function TaskDetailPanel() {
                     <div
                         className={`ctsp-add-task-grp`}
                     >
-                        <div className='ctsp-due-date-grp'>
-                        <input
-                            className='ctsp-date-input'
-                            type='date'
-                            value={dueDate}
-                            onChange={(e) => setDueDate(e.target.value)}
-                        />
-                        <select
-                            className='ctsp-time-select'
-                            value={dueTime}
-                            onChange={(e) => setDueTime(e.target.value)}
-                        >
-                            {selectMenuTimes.map((option) =>
-                                <option value={option.value}>{option.display}</option>
-                            )}
-                        </select>
+                        <div className='ctsp-top-half'>
+                            <div className='ctsp-top-left-grp'>
+                                <div className='ctsp-top-grp'>
+                                    <div className='ctsp-due-date-grp'>
+                                        <input
+                                            className='ctsp-date-input'
+                                            type='date'
+                                            value={dueDate}
+                                            onChange={(e) => setDueDate(e.target.value)}
+                                        />
+                                        <select
+                                            className='ctsp-time-select'
+                                            value={dueTime}
+                                            onChange={(e) => setDueTime(e.target.value)}
+                                        >
+                                            <option value=''>Due Time</option>
+                                            {selectMenuTimes.map((option) =>
+                                                <option value={option.value}>{option.display}</option>
+                                            )}
+                                        </select>
+                                    </div>
+                                    <div className='ctsp-due-date-grp'>
+                                        <input
+                                            className='ctsp-date-input'
+                                            type='date'
+                                            value={startDate}
+                                            onChange={(e) => setStartDate(e.target.value)}
+                                        />
+                                        <select
+                                            className='ctsp-time-select'
+                                            value={startTime}
+                                            onChange={(e) => setStartTime(e.target.value)}
+                                        >
+                                            <option value=''>Start Time</option>
+                                            {selectMenuTimes.map((option) =>
+                                                <option value={option.value}>{option.display}</option>
+                                            )}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className='ctsp-mid-grp-lf'>
+                                    <select
+                                        className='ctsp-time-select'
+                                        value={`${prio}`}
+                                        onChange={(e) => setPrio(Number(e.target.value))}
+                                    >
+                                        <option value=''>Priority</option>
+                                        <option value='1'>High</option>
+                                        <option value='2'>Med</option>
+                                        <option value='3'>Low</option>
+                                        <option value='0'>None</option>
+                                    </select>
+                                    <select
+                                        className='ctsp-time-select'
+                                        value={taskList}
+                                        onChange={(e) => setTaskList(e.target.value)}
+                                    >
+                                        <option value=''>List</option>
+                                        {Object.values(lists).map((l) =>
+                                            <option value={l.id}>{l.name}</option>
+                                        )}
+                                    </select>
+                                </div>
+                                <div className='ctsp-left-bot-grp'>
+                                    <input
+                                        className='ctsp-time-input'
+                                        type='number'
+                                        value={estimate}
+                                        onChange={(e) => setEstimate(e.target.value)}
+                                    />
+
+                                    <select
+                                        className='ctsp-time-select'
+                                        value={`${estimateUnit}`}
+                                        onChange={(e) => setEstimateUnit(Number(e.target.value))}
+                                    >
+                                        <option value='1'>Minutes</option>
+                                        <option value='60'>Hours</option>
+
+                                    </select>
+                                </div>
+                            </div>
+                            <div className='ctsp-tag-grp'>
+                                <select
+                                    className='ctsp-tag-input'
+                                    multiple
+                                    value={taskTags}
+                                    onChange={(e) => setTaskTags(
+                                        Array.from(e.target.selectedOptions).map((el) => (
+                                            el.value)))}
+                                >
+                                    <optgroup label='Tags' />
+                                    {Object.values(tags).map((t) =>
+                                        <option className='sel-op' value={t.id}>{t.name}</option>
+                                    )}
+                                </select>
+                            </div>
                         </div>
-                        <input
-                            className='ctsp-date-input'
-                            type='date'
-                            value={startDate}
-                            onChange={(e) => setStartDate(e.target.value)}
-                        />
-                        <select
-                            className='ctsp-time-select'
-                            value={startTime}
-                            onChange={(e) => setStartTime(e.target.value)}
-                        >
-                            {selectMenuTimes.map((option) =>
-                                <option value={option.value}>{option.display}</option>
-                            )}
-                        </select>
+                        <div className='ctsp-at-btn-div'>
+                            <div className={`ctsp-add-task-grp-icons`}>
+                                <img
+                                    className='ctsp-at-icon-style'
+                                    src={dueDateIcon}
+                                />
 
-                        <select
-                            className='ctsp-prio-input'
-                            value={`${prio}`}
-                            onChange={(e) => setPrio(Number(e.target.value))}
-                        >
-                            <option value=''>Priority</option>
-                            <option value='1'>High</option>
-                            <option value='2'>Med</option>
-                            <option value='3'>Low</option>
-                            <option value='0'>None</option>
-                        </select>
-                        <select
-                            className='ctsp-list-input'
-                            value={taskList}
-                            onChange={(e) => setTaskList(e.target.value)}
-                        >
-                            <option value=''>List</option>
-                            {Object.values(lists).map((l) =>
-                                <option value={l.id}>{l.name}</option>
-                            )}
-                        </select>
-                        <select
-                            className='ctsp-tag-input'
-                            multiple
-                            value={taskTags}
-                            onChange={(e) => setTaskTags(
-                                Array.from(e.target.selectedOptions).map((el) => (
-                                    el.value)))}
-                        >
-                            {Object.values(tags).map((t) =>
-                                <option value={t.id}>{t.name}</option>
-                            )}
-                        </select>
-                        <input
-                            type='number'
-                            value={estimate}
-                            onChange={(e) => setEstimate(e.target.value)}
-                        />
-
-                        <select
-                            className='ctsp-prio-input'
-                            value={`${estimateUnit}`}
-                            onChange={(e) => setEstimateUnit(Number(e.target.value))}
-                        >
-                            <option value='1'>Minutes</option>
-                            <option value='60'>Hours</option>
-
-                        </select>
-
-                        <button
-                            className='ctsp-ct-submit-btn'
-                            type='submit'
-                            disabled={taskName.length ? false : true}
-                        >
-                            Update Task
-                        </button>
+                                <img className='ctsp-at-icon-style' src={startDateIcon} />
+                                <img className='ctsp-at-icon-style' src={listIcon} />
+                                <img className='ctsp-at-icon-style' src={prioIcon} />
+                                {/* <img className='ctsp-at-icon-style' src={repeatIcon} />
+                            <img className='ctsp-at-icon-style' src={locationPin} /> */}
+                                <img className='ctsp-at-icon-style' src={clockIcon} />
+                            </div>
+                            <button
+                                className='ctsp-ct-submit-btn'
+                                type='submit'
+                                disabled={taskName.length ? false : true}
+                            >
+                                Add Task
+                            </button>
+                        </div>
                     </div>
 
-                    {/* </div> */}
                 </div >
             </form >
         </div>
