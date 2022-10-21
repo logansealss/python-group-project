2650557c4448d827ea5a6c1c82767354e30989ec
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-10-21 12:48:37 -0500

    added color selector to tag create and update form

--- react-app/src/components/TaskApp/TaskAppSidebar/Forms/CreateTagListForm.js
+++ react-app/src/components/TaskApp/TaskAppSidebar/Forms/CreateTagListForm.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ColorDropdown from "./ColorDropdown"
 import "./CreateTagListForm.css"
 
 export default function CreateListTagForm(props) {
   const dispatch = useDispatch()
   const [name, setName] = useState('')
-  const [validationErr, setValidationErr] = useState()
+  const [color, setColor] = useState('#006400')
+  const [validationErr, setValidationErr] = useState('')
 
   const userId = useSelector(state => state.session.user.id);
 
@@ -18,15 +20,22 @@ export default function CreateListTagForm(props) {
       setValidationErr("Name is too long. Please choose a shorter name.")
     } else {
       setValidationErr()
-      const response = await dispatch(props.thunk({
+
+      const dataObj = {
         'user_id': userId,
-        'name': name
-      }));
+        'name': name,
+      }
+
+      if(props.feature === "tag"){
+        dataObj.color = color;
+      }
+
+      const response = await dispatch(props.thunk(dataObj));
       if (response.ok) {
         props.setShowModal(false);
       } else {
         let data = await response.json()
-        if (data.errors){
+        if (data.errors) {
           setValidationErr(`You already have a ${props.feature} with this name. Please choose another name.`)
         }
       }
@@ -42,10 +51,10 @@ export default function CreateListTagForm(props) {
       <div
         id="form-content-container"
       >
-        <label>Please enter a new {props.feature} name</label>
+        <label for="modal_text_input_name">Please enter a new {props.feature} name</label>
         <input
-          id='modal_text_input'
-          className={validationErr ? 'modal-text-err': ''}
+          id='modal_text_input_name'
+          className={validationErr ? 'modal-text-err' : ''}
           type='text'
           onChange={e => setName(e.target.value)}
           autoFocus
@@ -55,6 +64,9 @@ export default function CreateListTagForm(props) {
           <div id="modal-err">
             {validationErr}
           </div>}
+
+          {props.feature === "tag" && <ColorDropdown color={color} setColor={setColor}/>}
+
       </div>
       <div id='modal_buttons'>
         <button id='modal_submit' type='submit'>Add</button>
