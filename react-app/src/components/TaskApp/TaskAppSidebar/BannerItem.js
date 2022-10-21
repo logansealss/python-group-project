14127b00a48dd1d3ea93f878ac921db59581d7e3
JohnCarrera <johnrcarrera@gmail.com>
2022-10-20 23:22:08 -0400

    added tag colors to sidebar with default color that matches the dark green used elsewhere

--- react-app/src/components/TaskApp/TaskAppSidebar/BannerItem.js
+++ react-app/src/components/TaskApp/TaskAppSidebar/BannerItem.js
@@ -3,9 +3,13 @@ import { DropdownProvider } from '../../../context/Dropdown';
 
 export default function BannerItem (props) {
 
+    console.log('props=====', props)
+
+
   return (
     <DropdownProvider position='relative'>
       <div id='banner_item'>
+        {props.color ? <div className='sb-tag-color' style={{backgroundColor: props.color}}></div> : null }
         <div className='title' onClick={props.handleClick}>{props.children}</div>
         <div className='collapser_rhs_icons'>
           {props.obj}
