603a05aab0befd793f88d43423581047aa830847
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-10-21 02:11:15 -0500

    started validations

--- react-app/src/components/TaskApp/TaskAppSidebar/Forms/CreateTagListForm.js
+++ react-app/src/components/TaskApp/TaskAppSidebar/Forms/CreateTagListForm.js
@@ -5,37 +5,53 @@ import "./CreateTagListForm.css"
 export default function CreateListTagForm(props) {
   const dispatch = useDispatch()
   const [name, setName] = useState('')
+  const [validationErr, setValidationErr] = useState()
 
   const userId = useSelector(state => state.session.user.id);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await dispatch(props.thunk({
-      'user_id': userId,
-      'name': name
-    }));
-    if (response.ok) {
-      props.setShowModal(false);
-    }
 
+    if (name.length < 1) {
+      setValidationErr("No name entered. Please choose a name.")
+    } else if (name.length > 150) {
+      setValidationErr("Name is too long. Please choose a shorter name.")
+    } else {
+      setValidationErr()
+      const response = await dispatch(props.thunk({
+        'user_id': userId,
+        'name': name
+      }));
+      if (response.ok) {
+        props.setShowModal(false);
+      } else {
+        console.log(response)
+      }
+    }
   };
 
 
   return (
-    <form 
+    <form
       onSubmit={handleSubmit}
-      style={{width: '300px'}}
+      style={{ width: '300px' }}
     >
-      <div 
+      <div
         id="form-content-container"
       >
         <label>Please enter a new {props.feature} name</label>
         <input
           id='modal_text_input'
+          className={validationErr ? 'modal-text-err': ''}
           type='text'
           onChange={e => setName(e.target.value)}
           autoFocus
         />
+
+        {validationErr &&
+          <div id="modal-err">
+            {validationErr}
+          </div>}
       </div>
       <div id='modal_buttons'>
         <button id='modal_submit' type='submit'>Add</button>
--- react-app/src/components/TaskApp/TaskAppSidebar/Forms/RenameTagListForm.js
+++ react-app/src/components/TaskApp/TaskAppSidebar/Forms/RenameTagListForm.js
@@ -9,6 +9,7 @@ export default function RenameListTagForm(props) {
   const dispatch = useDispatch()
   const [name, setName] = useState(props.name)
   const [visited, setVisited] = useState(false)
+  const [validationErr, setValidationErr] = useState()
 
   const actions = {
     'list': listActions.renameList,
@@ -19,27 +20,36 @@ export default function RenameListTagForm(props) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await dispatch(actions[props.feature]({
-      'id': props.itemId,
-      'name': name
-    }));
-    if (response.ok) {
-      props.setShowModal(false);
-    }
 
+    if (name.length < 1) {
+      setValidationErr("No name entered. Please choose a name.")
+    } else if (name.length > 150) {
+      setValidationErr("Name is too long. Please choose a shorter name.")
+    } else {
+      setValidationErr()
+      const response = await dispatch(actions[props.feature]({
+          'id': props.itemId,
+          'name': name
+        }));
+      if (response.ok) {
+        props.setShowModal(false);
+      } else {
+        console.log(response)
+      }
+    }
   };
 
   const handleFocus = (event) => {
-    if(!visited){
+    if (!visited) {
       event.target.select();
       setVisited(true)
     }
   }
 
   return (
-    <form 
+    <form
       onSubmit={handleSubmit}
-      style={{width: '300px'}}
+      style={{ width: '300px' }}
     >
       <div
         id="form-content-container"
@@ -48,12 +58,18 @@ export default function RenameListTagForm(props) {
         <br></br>
         <input
           id='modal_text_input'
+          className={validationErr ? 'modal-text-err': ''}
           type='text'
           value={name}
           onChange={e => setName(e.target.value)}
           autoFocus
           onFocus={handleFocus}
         />
+
+        {validationErr &&
+          <div id="modal-err">
+            {validationErr}
+          </div>}
       </div>
       <div id='modal_buttons'>
         <button id='modal_submit' type='submit'>Save</button>
